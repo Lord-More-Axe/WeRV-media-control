810e2f80850f26114451f2898eb33bc3355c5e7b
sadra20012 <61895507+sadra20012@users.noreply.github.com>
2023-01-07 19:51:01 +0800

    changes

---
+++ static/js/main.js
@@ -0,0 +1,147 @@
+let now_playing = document.querySelector(".now-playing");
+let track_art = document.querySelector(".track-art");
+let track_name = document.querySelector(".track-name");
+let track_artist = document.querySelector(".track-artist");
+
+let playpause_btn = document.querySelector(".playpause-track");
+let next_btn = document.querySelector(".next-track");
+let prev_btn = document.querySelector(".prev-track");
+
+let seek_slider = document.querySelector(".seek_slider");
+let volume_slider = document.querySelector(".volume_slider");
+let curr_time = document.querySelector(".current-time");
+let total_duration = document.querySelector(".total-duration");
+
+let track_index = 0;
+let isPlaying = false;
+let updateTimer;
+
+// Create new audio element
+let curr_track = document.createElement('audio');
+
+// Define the tracks that have to be played
+let track_list = [
+  {
+    name: "Night Owl",
+    artist: "Broke For Free",
+    image: "https://images.pexels.com/photos/2264753/pexels-photo-2264753.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=250&w=250",
+    path: "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/WFMU/Broke_For_Free/Directionless_EP/Broke_For_Free_-_01_-_Night_Owl.mp3"
+  },
+  {
+    name: "Enthusiast",
+    artist: "Tours",
+    image: "https://images.pexels.com/photos/3100835/pexels-photo-3100835.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=250&w=250",
+    path: "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/no_curator/Tours/Enthusiast/Tours_-_01_-_Enthusiast.mp3"
+  },
+  {
+    name: "Shipping Lanes",
+    artist: "Chad Crouch",
+    image: "https://images.pexels.com/photos/1717969/pexels-photo-1717969.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=250&w=250",
+    path: "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/ccCommunity/Chad_Crouch/Arps/Chad_Crouch_-_Shipping_Lanes.mp3",
+  },
+];
+
+function random_bg_color() {
+
+  // Get a number between 64 to 256 (for getting lighter colors)
+  let red = Math.floor(Math.random() * 256) + 64;
+  let green = Math.floor(Math.random() * 256) + 64;
+  let blue = Math.floor(Math.random() * 256) + 64;
+
+  // Construct a color withe the given values
+  let bgColor = "rgb(" + red + "," + green + "," + blue + ")";
+
+  // Set the background to that color
+  document.body.style.background = bgColor;
+}
+
+function loadTrack(track_index) {
+  clearInterval(updateTimer);
+  resetValues();
+  curr_track.src = track_list[track_index].path;
+  curr_track.load();
+
+  track_art.style.backgroundImage = "url(" + track_list[track_index].image + ")";
+  track_name.textContent = track_list[track_index].name;
+  track_artist.textContent = track_list[track_index].artist;
+  now_playing.textContent = "PLAYING " + (track_index + 1) + " OF " + track_list.length;
+
+  updateTimer = setInterval(seekUpdate, 1000);
+  curr_track.addEventListener("ended", nextTrack);
+  random_bg_color();
+}
+
+function resetValues() {
+  curr_time.textContent = "00:00";
+  total_duration.textContent = "00:00"; 
+  seek_slider.value = 0;
+}
+
+// Load the first track in the tracklist
+loadTrack(track_index);
+
+function playpauseTrack() {
+  // if (!isPlaying) playTrack();
+  // else pauseTrack();
+                $.ajax({
+                    method: "post",
+                    url: "/pp",
+                    data: {
+                        text: ''
+                    },
+                    success: function(res) {
+                      if (!isPlaying) playTrack();
+                      else pauseTrack();
+                    }
+                });
+}
+
+function playTrack() {
+  // curr_track.play();
+  isPlaying = true;
+  playpause_btn.innerHTML = '<i class="fa fa-pause-circle fa-5x"></i>';
+}
+
+function pauseTrack() {
+  // curr_track.pause();
+  isPlaying = false;
+  playpause_btn.innerHTML = '<i class="fa fa-play-circle fa-5x"></i>';;
+}
+
+function nextTrack() {
+  $.ajax({
+    method: "post",
+    url: "/next",
+    data: {
+        text: ''
+    },
+    success: function(res) {
+      console.log('nexed')
+    }
+});
+}
+
+function prevTrack() {
+  function nextTrack() {
+    $.ajax({
+      method: "post",
+      url: "/pre",
+      data: {
+          text: ''
+      },
+      success: function(res) {
+        console.log('pred')
+      }
+  });
+  }
+}
+
+
+
+function setVolume() {
+  curr_track.volume = volume_slider.value / 100;
+}
+
+
+
+
--- werv.py
+++ werv.py
@@ -1,10 +1,78 @@
 from flask import Flask, render_template, request, make_response, current_app, redirect, url_for, flash, session, logging, jsonify, abort
+import win32api
+import ctypes
+import asyncio
+from PIL import Image
+import io
+import array
+
+VK_MEDIA_PLAY_PAUSE = 0xB3
+VK_MEDIA_NEXT_TRACK = 0xB0
+VK_MEDIA_PREV_TRACK = 0xB1
+hwcode = win32api.MapVirtualKey(VK_MEDIA_PLAY_PAUSE, 0)
+from winsdk.windows.media.control import \
+    GlobalSystemMediaTransportControlsSessionManager as MediaManager
+
+from winsdk.windows.storage.streams import DataReader, Buffer, InputStreamOptions
+
+async def get_media_info():
+    sessions = await MediaManager.request_async()
+
+
+    current_session = sessions.get_current_session()
+    info = await current_session.try_get_media_properties_async()
+
+
+    info_dict = {song_attr: info.__getattribute__(song_attr) for song_attr in dir(info) if song_attr[0] != '_'}
+
+
+    info_dict['genres'] = list(info_dict['genres'])
+
+
+    print(info_dict)
+    return info_dict
+
+
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template('main.html')
+    return render_template('index.html')
+
+
+@app.route("/pp", methods=["POST","GET"])
+def playpause():
+    print('pped')
+    win32api.keybd_event(VK_MEDIA_PLAY_PAUSE, win32api.MapVirtualKey(VK_MEDIA_PLAY_PAUSE, 0))
+    
+    info = asyncio.run(get_media_info())
+    thumbnail = info['thumbnail']
+
+    async def read_stream_into_buffer(stream_ref, buffer):
+        readable_stream = await stream_ref.open_read_async()
+        await readable_stream.read_async(buffer, buffer.capacity, InputStreamOptions.READ_AHEAD)
+
+    thumb_read_buffer = Buffer(5000000)
+    asyncio.run(read_stream_into_buffer(thumbnail, thumb_read_buffer))
+    mv = memoryview(thumb_read_buffer)
+    byte_array = bytearray(mv)
+    image_bytes = io.BytesIO(bytes(byte_array))
+    image = Image.open(image_bytes)
+    image.show()
+
+
+@app.route("/next", methods=["POST","GET"])
+def next():
+    print('nexed')
+    win32api.keybd_event(VK_MEDIA_NEXT_TRACK, win32api.MapVirtualKey(VK_MEDIA_NEXT_TRACK, 0))
+
+
+@app.route("/pre", methods=["POST","GET"])
+def pre():
+    print('pred')
+    win32api.keybd_event(VK_MEDIA_PREV_TRACK, win32api.MapVirtualKey(VK_MEDIA_PREV_TRACK, 0))
 
 if __name__ == '__main__':
     app.run(debug=True)
